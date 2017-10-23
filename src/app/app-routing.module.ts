8d9ab1725950b25e8a48b758850f2f0201221e69
Sergej Werfel <sergej.werfel@andrena.de>
2017-10-23 15:22:46 +0200

    Reload timebox, when clicked on the menu for already running timebox

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import {APP_BASE_HREF} from '@angular/common';
 import { NgModule }             from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, Router, NavigationEnd } from '@angular/router';
 
 import { RunningTimeboxComponent } from './running-timebox.component';
 import { environment } from './../environments/environment';
@@ -14,4 +14,20 @@ const routes: Routes = [
 	exports: [ RouterModule ],
     providers: [{provide: APP_BASE_HREF, useValue : environment.basehref }]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+  constructor(private router: Router){
+    // override the route reuse strategy
+    this.router.routeReuseStrategy.shouldReuseRoute = function(){
+      return false;
+    }
+
+    this.router.events.subscribe((evt) => {
+      if (evt instanceof NavigationEnd) {
+        // trick the Router into believing it's last link wasn't previously loaded
+        this.router.navigated = false;
+        // if you need to scroll back to top, here is the right place
+        window.scrollTo(0, 0);
+      }
+    });
+  }
+}
